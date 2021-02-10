c6ad65c8d60a0b78aed92f986da58244ba3aa0e8
showetek <t.heine02@hotmail.com>
2021-02-10 23:01:06 +0100

    little code clean up | SVGs | updated README

--- src/FanStation/FanStation.ino
+++ src/FanStation/FanStation.ino
@@ -1,21 +1,21 @@
 //Libs
 #include <SoftwareSerial.h>
 
+//SoftwareSerial
 SoftwareSerial komm(4, 3);
 
+//Var
 int pwm = 0;
-//pinMode(Output, 9);
 
+//Setup
 void setup() {
  komm.begin(115200);
  Serial.begin(9600);
-
 }
 
-
+//Mainloop des Hauptprogrammes
 void loop() {
-  
-  pwm = komm.read();
+   pwm = komm.read();
    Serial.println(pwm);
    analogWrite(9,pwm);
    delay(5000);
